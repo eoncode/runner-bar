94d661b901a9f8aa222d072965bd431d409f8cf7
André J <andre@sentry.co>
2026-05-16 02:59:06 +0200

    fix(lint): remove superfluous disable:next so /// lands directly above enum Phase

--- Sources/RunnerBar/ReRunButton.swift
+++ Sources/RunnerBar/ReRunButton.swift
@@ -17,7 +17,6 @@ struct ReRunButton: View {
     // MARK: - Phase
 
     /// Visual states of the re-run button lifecycle.
-    // swiftlint:disable:next missing_docs
     enum Phase {
         /// Normal tappable state.
         case idle
