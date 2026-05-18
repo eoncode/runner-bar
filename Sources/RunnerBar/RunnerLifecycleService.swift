a578dad40dced02909c2f5fcfbfd9005d2cc14d3
André J <andre@sentry.co>
2026-05-18 02:16:37 +0200

    fix(lint): add targeted type_body_length disable in RunnerLifecycleService.swift

--- Sources/RunnerBar/RunnerLifecycleService.swift
+++ Sources/RunnerBar/RunnerLifecycleService.swift
@@ -12,6 +12,7 @@ enum LifecycleResult {
 
 // MARK: - RunnerLifecycleService
 
+// swiftlint:disable:next type_body_length
 struct RunnerLifecycleService {
     static let shared = RunnerLifecycleService()
     private init() {}
