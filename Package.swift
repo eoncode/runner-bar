edc54e3dc9095ecf425daf353ff7839115e4df78
eonist <30n1st@gmail.com>
2026-05-24 06:27:04 +0200

    feat: bump deployment target to macOS 26 for Liquid Glass (#813)

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "RunnerBar",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v26)],
     targets: [
         .target(
             name: "RunnerBarCore",
