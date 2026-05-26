21638429431fb28fed238facfa3a065a1277db12
André J <andre@sentry.co>
2026-05-26 02:06:47 +0200

    fix: expose RunnerBarCore as a library product in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 let package = Package(
     name: "RunnerBar",
     platforms: [.macOS(.v26)],
+    products: [
+        .library(
+            name: "RunnerBarCore",
+            targets: ["RunnerBarCore"]
+        )
+    ],
     targets: [
         .target(
             name: "RunnerBarCore",
